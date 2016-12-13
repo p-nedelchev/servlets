0f6facb725378a4637a40c32c298e2ef8edfa061
Petar Nedelchev <peter.krasimirov@gmail.com>
2016-12-13 09:40:05 +0200

    Proper test name and fix

--- src/main/java/Jetty.java
+++ src/main/java/Jetty.java
@@ -26,7 +26,7 @@ public final class Jetty {
             public void contextInitialized(ServletContextEvent servletContextEvent) {
                 ServletContext servletContext = servletContextEvent.getServletContext();
 
-                servletContext.addServlet("chainletServlet", new Chainlet()).addMapping("/page1", "/page2", "/page3");=======
+                servletContext.addServlet("chainletServlet", new Chainlet()).addMapping("/page1", "/page2", "/page3");
                 servletContext.addServlet("counterServlet", new Counter()).addMapping("/counter");
             }
 
